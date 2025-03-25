caa67f6add9d2541a73c04c152815a61b637ef54
Suriya2023 <rajputsuraj221221@gmail.com>
2025-03-25 17:19:50 +0530

    Make a Aplication

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { registerSW } from 'virtual:pwa-register'
 import './index.css'
 import App from './App.jsx'
+
+// Register service worker
+const updateteSw = registerSW({
+  noNeedRefresh() {
+    if (confirm('Are you sure you want to')) {
+      updateteSw(true);
+    }
+  }
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,57 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+  VitePWA(
+    {
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'E-Commerce Shopping App',
+        short_name: 'E-Commerce',
+        description: 'A simple e-commerce shopping app built with React and Vite',
+        theme_color: '#3182CE',
+        background_color: '#F5F5F5',
+        display: 'standalone',
+        start_url: '/ecommerce/',
+
+        icons: [
+
+          {
+            src: '/icon_192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+
+            src: '/icon_512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+
+
+        ],
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            // https://suriya2023.github.io/ecommerce/homeLeaving
+            urlPattern: /^https:\/\/suriya2023\.github\.io\/ecommerce\/.*/,
+            handler: 'networkFirst',
+            options: {
+              cacheName: 'api-cache',
+              expiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 60 * 60 * 24, // 24 hours
+              },
+            },
+          },
+        ],
+      },
+
+    },
+  )
+  ],
   base: '/ecommerce/', // Replace 'your-repo-name' with your GitHub repository name
 })
